ba2cd3c935a9bca831341d8a5bfdc364e43756e1
AnTran <144910526+AnTranQuoc@users.noreply.github.com>
2024-07-14 21:47:03 +0700

    push

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Languages: CSS, TypeScript, JavaScript, Python, C#, C++</li>
+        <li>Libraries & Frameworks: ReactJS, ReactNative, React Hooks, Material UI, Vitejs, TailwindCSS, Next.js</li>
+        <li>Design Tools: Photoshop, canva</li>
+        <li>Other: Git, Restful API, SQLsever, GraphQL</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>University of Information Technology - UIT (2021 – present)</li>
+                  <li>Major: Computer Science</li>
+                  <li>GPA: 8/10</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+         <li>Equivalent to 650 Toeic</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/TQA.jpg" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+           A web developer proficient in CSS, JavaScript, React. Possessing a fundamental understanding
+           of Three.js for 3D manipulation and Leaflet for geographic map handling. I am driven to utilize
+           my knowledge, skills, and programming experience to craft software with exceptional features,
+           enhancing the company's reputation and attracting a diverse customer base.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,128 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Car Showroom 3D with Three.JS",
+    description: "Develop a professional car showroom project targeting luxury car dealerships. Utilize small-scale 3D models with numerous features for customers. The website aims to provide a comfortable experience for customers, making it easy for them to choose their favorite car.",
+    image: "/images/projects/1.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/AnTranQuoc/CarShowroom",
+    previewUrl: "https://github.com/AnTranQuoc/CarShowroom",
+  },
+  {
+    id: 2,
+    title: "3D maze game with Three.JS",
+    description: "A simple maze game with first-person perspective. Using Javascript, HTML combined with ThreeJS.",
+    image: "/images/projects/2.png",
+    tag: ["All", "Game"],
+    gitUrl: "https://github.com/AnTranQuoc/MazeWebGame",
+    previewUrl: "https://antranquoc.github.io/MazeWebGame/",
+  },
+  {
+    id: 3,
+    title: "Movie website built on React ⚛️ ",
+    description: "A movie website was developed using the React framework. It was necessary to use fundamental concepts of HTML and CSS to build the page and style.",
+    image: "/images/projects/3.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/AnTranQuoc/Movie_Web",
+    previewUrl: "https://github.com/AnTranQuoc/Movie_Web",
+  },
+  {
+    id: 4,
+    title: "Personal portfolio by Next.JS",
+    description: "Developed a dynamic and visually appealing portfolio using Next.js. The unique design and smooth effects showcase technical skills and creativity in web development.",
+    image: "/images/projects/4.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "Asteroids game made with JavaScript.",
+    description: "This is a 2D game made in JavaScript. The player must control a ship, trying to fend off planets rushing at it. If you fail and get hit by a planet, the game will end.",
+    image: "/images/projects/5.png",
+    tag: ["All", "Game"],
+    gitUrl: "https://github.com/AnTranQuoc/Asteroids_game",
+    previewUrl: "https://antranquoc.github.io/Asteroids_game/",
+  },
+  {
+    id: 6,
+    title: "3D Iphone display Website.",
+    description: "Develop a professional iPhone product promotion website project using React-Three-Fiber. Use 3D models with color changing features. Mainly using react and threeJS",
+    image: "/images/projects/6.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/AnTranQuoc/3D_Iphone_Display",
+    previewUrl: "https://github.com/AnTranQuoc/3D_Iphone_Display",
+  }
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Game"
+          isSelected={tag === "Game"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
